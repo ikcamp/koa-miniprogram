ce9a87b0707c06df7846994b5524f71312279213
brucecham <brucecham@qq.com>
2018-05-29 18:38:31 +0800

    修改查询方式

--- koa-service/lib/db/photo.js
+++ koa-service/lib/db/photo.js
@@ -4,36 +4,31 @@ const {
 } = require('./model')
 
 module.exports = {
-  async add (openId, url, albumId) {
+  async add(openId, url, albumId) {
     let _photo = await Phopto.create({
       openId,
       url,
       albumId
     })
-    await Album.update({
-      _id: albumId
-    }, {
-      photoId: _photo._id
-    })
     return _photo
   },
-  async approve (id) {
+  async approve(id) {
     return Phopto.update({
       _id: id
     }, {
       isApproved: true
     })
   },
-  async delete (id) {
+  async delete(id) {
     return Phopto.update({
       _id: id
     }, {
       isDelete: true
     })
   },
-  async getPhotos (openId, albumId, pageIndex, pageSize) {
-    let result 
-    if(pageSize){
+  async getPhotos(openId, albumId, pageIndex, pageSize) {
+    let result
+    if (pageSize) {
       result = await Phopto.find({
         openId,
         albumId,
@@ -42,7 +37,7 @@ module.exports = {
       }).sort({
         'created': -1
       }).skip((pageIndex - 1) * pageSize).limit(pageSize)
-    }else{
+    } else {
       result = result = await Phopto.find({
         openId,
         albumId,
@@ -54,30 +49,32 @@ module.exports = {
     }
     return result
   },
-  async getPhotosByAlbumId (albumId, pageIndex, pageSize) {
+  async getPhotosByAlbumId(albumId, pageIndex, pageSize) {
     let result
-    if(pageSize){
+    if (pageSize) {
       result = await Phopto.find({
         albumId,
         isApproved: true,
         isDelete: false
       }).skip((pageIndex - 1) * pageSize).limit(pageSize)
-    }else{
+    } else {
       result = await Phopto.find({
         albumId,
         isApproved: true,
         isDelete: false
+      }).sort({
+        'updated': -1
       })
     }
     return result
   },
-  async getApprovingPhotos (pageIndex, pageSize) {
+  async getApprovingPhotos(pageIndex, pageSize) {
     return Phopto.find({
       isApproved: false,
       isDelete: false
     }).skip((pageIndex - 1) * pageSize).limit(pageSize)
   },
-  async getPhotoById (id) {
+  async getPhotoById(id) {
     return Phopto.findById(id)
   }
 }
