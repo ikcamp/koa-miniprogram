9958cffb4f014d111995bbcd68ad19e1bc138e53
戴亮 <dailiang@dailiangdeMacBook-Pro.local>
2018-05-29 11:49:03 +0800

    update: 修复权限校验

--- koa-service/lib/crypto.js
+++ koa-service/lib/crypto.js
@@ -1,25 +1,32 @@
 const crypto = require('crypto')
 const secret = 'ikcamp_2018_06'
 const algorithm = 'aes-256-cbc'
-module.exports = {
-  encode(id) {
-    const encoder = crypto.createCipher(algorithm, secret)
-    const str = [id, Date.now(), 'ikcamp2018'].join('|')
-    let encrypted = encoder.update(str, 'utf8', 'hex')
-    encrypted += encoder.final('hex')
-    return encrypted
-  },
-  decode(str) {
-    const decoder = crypto.createDecipher(algorithm, secret)
-    let decoded = decoder.update(str, 'hex', 'utf8')
-    decoded += decoder.final('utf8')
-    const arr = decoded.split('|')
-    return {
-      id: parseInt(arr[0]),
-      timespan: parseInt(arr[1])
-    }
-  },
-  encodeErCode() {
-    return this.encode(Math.random())
+
+function encode (id) {
+  const encoder = crypto.createCipher(algorithm, secret)
+  const str = [id, Date.now(), 'ikcamp2018'].join('|')
+  let encrypted = encoder.update(str, 'utf8', 'hex')
+  encrypted += encoder.final('hex')
+  return encrypted
+}
+
+function decode (str) {
+  const decoder = crypto.createDecipher(algorithm, secret)
+  let decoded = decoder.update(str, 'hex', 'utf8')
+  decoded += decoder.final('utf8')
+  const arr = decoded.split('|')
+  return {
+    id: parseInt(arr[0]),
+    timespan: parseInt(arr[1])
   }
+}
+
+function encodeErCode () {
+  return encode(Math.random())
+}
+
+module.exports = {
+  encode,
+  decode,
+  encodeErCode
 }
