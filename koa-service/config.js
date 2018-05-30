cc901c2ff6b064fe7163a5e0028dcf054431bb66
brucecham <brucecham@qq.com>
2018-05-30 20:40:57 +0800

    back config

--- koa-service/config.js
+++ koa-service/config.js
@@ -3,15 +3,15 @@ const appKey = env.APP_KET || 'default key'
 const appSecret = env.APP_SECRET || 'default secret'
 const nodeEnv = env.NODE_ENV
 let db = {
-  name: 'mongodb://127.0.0.1:27017/xcx?authSource=admin',
-  user: 'xcx',
-  password: '123qwe'
+  name: 'mongodb://127.0.0.1:27017/xcx',
+  user: 'user',
+  password: 'pass'
 }
 if (nodeEnv === 'production') {
   db = {
     name: 'mongodb://127.0.0.1:27017/xcx',
-    user: 'xcx',
-    password: '123qwe'
+    user: 'user',
+    password: 'pass'
   }
 }
 
