351296a1957c05d5b8f3bd806dfedfcdd454b63b
zhouyao <zhouyao@zhouyaodeMacBook-Pro.local>
2018-05-31 19:12:32 +0800

    udpate album

--- miniprogram/pages/pics/pics.js
+++ miniprogram/pages/pics/pics.js
@@ -6,60 +6,41 @@ const mapStateToProps = (state) => {
     pics: state.pics
   }
 }
+
 Page(connect(mapStateToProps)({
   data: {
     hidden: true,
     Host: SERVER.HOST,
     fm: SERVER.FM
   },
-  onLoad() {
-
+  onShow(){
     this.getPics()
-
   },
   getPics() {
-
     SERVER.getPics().then(res => {
-      const _data = res.data
-
-      if (_data.status == 0) {
-        this.__dispatch({
-          type: "MODIFY_PICS",
-          data: _data.data || []
-        })
-      }
+      this.setData({ pics: res.data.data })
     })
-
   },
   create() {
     this.setData({ hidden: false })
   },
   onAddPics(e) {
-    const { name } = e.detail
 
-    wx.showLoading({ title: '网络连接中', mask: true })
+    wx.showLoading({ title: '提交中...', mask: true })
 
-    SERVER.addPics(name).then(res => {
-
+    SERVER.addPics(e.detail.name).then(res => {
       if (res.data.status == 0) this.getPics()
-
     }).finally(() => {
-
       wx.hideLoading()
       this.setData({ hidden: true })
-
     })
   },
   onGoBack() {
-
     wx.hideLoading()
     this.setData({ hidden: true })
-
   },
   toDetail(evt) {
-
     let { id, name } = evt.currentTarget.dataset
-
     wx.navigateTo({ url: `../pic/pic?id=${id}&name=${name}` })
   }
 }))
